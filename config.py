75866c9329ff0d62baa6b324605f295390152bbe
Shruti0906 <102983097+Shruti0906@users.noreply.github.com>
2022-04-05 12:33:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-customhost = "mydatabase.ci4td1u42qon.us-east-1.rds.amazonaws.com"
+customhost = mydb.copdgllh7tt1.us-east-2.rds.amazonaws.com
 customuser = "admin"
-custompass = "admin123"
-customdb = "mydatabase"
-custombucket = "my-project2-bucket"
-customregion = "us-east-1"
+custompass = "yashu0000"
+customdb = "mydb"
+custombucket = "my-project02"
+customregion = "us-east-2"
